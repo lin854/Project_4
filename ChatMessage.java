67d6e58b73efaf764fa49586e8fa7a7266febeab
Sam Lin <lin854@purdue.edu>
2017-11-17 04:56:11 -0500

    Formatting issues.

--- ChatMessage.java
+++ ChatMessage.java
@@ -2,11 +2,13 @@ import java.io.Serializable;
 
 final class ChatMessage implements Serializable {
     private static final long serialVersionUID = 6898543889087L;
-    private int type;
+    private int type; //0 = general; 1 = logout; 2 = DM
     private String message;
-    public ChatMessage(int type, String message){
+    private String username;
+    public ChatMessage(int type, String message, String username){
         this.type = type;
         this.message = message;
+        this.username = username;
     }
     public int getType(){
         return type;
@@ -14,4 +16,5 @@ final class ChatMessage implements Serializable {
     public String getMessage(){
         return message;
     }
+    public String getRecipient(){return username;}
 }
